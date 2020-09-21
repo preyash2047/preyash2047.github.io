de8bafec76728aab866f9ba35a558b776d20c709
Preyash Patel <preyash2047@GMAIL.com>
2020-09-21 20:10:50 +0530

    test

---
+++ home/index.js
@@ -0,0 +1,35 @@
+let net;
+const webcamElement = document.getElementById('webcam');
+
+// JavaScript
+import * as tf from 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs';
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  //net = await mobilenet.load();
+  //const model = await tf.loadLayersModel('https://foo.bar/tfjs_artifacts/model.json');
+  net = await tf.loadLayersModel('https://foo.bar/tfjs_artifacts/model.json');
+  console.log('Successfully loaded model');
+
+  // Create an object from Tensorflow.js data API which could capture image
+  // from the web camera as Tensor.
+  const webcam = await tf.data.webcam(webcamElement);
+  while (true) {
+    const img = await webcam.capture();
+    const result = await net.classify(img);
+
+    document.getElementById('console').innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+    // Dispose the tensor to release the memory.
+    img.dispose();
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
+  }
+}
+
+app();
